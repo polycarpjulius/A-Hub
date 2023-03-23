36b3d2708daec45a5216ee4a48e83d5aae97c398
julius <juliuspolycarp93@gmail.com>
2023-03-23 03:38:19 +0100

    udating my webpage

--- JsFile/index.js
+++ JsFile/index.js
@@ -1,3 +1,5 @@
-console.log('Welcome to my new js file');
-var name = 'julius';
-console.log('hi my name is ', name);
+function echo() {
+    console.log('Welcome to my new js file');
+    var name = 'julius';
+    alert('hi my name is ' + name + 'please how can i help you?');
+}
